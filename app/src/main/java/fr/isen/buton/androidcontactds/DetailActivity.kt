ebcae68e654f6776b217b5068077ee30b66a86af
Tomitom13 <thomas.buton@isen.yncrea.Fr>
2023-01-27 15:58:21 +0100

    not working

--- app/src/main/java/fr/isen/buton/androidcontactds/DetailActivity.kt
+++ app/src/main/java/fr/isen/buton/androidcontactds/DetailActivity.kt
@@ -3,9 +3,11 @@ package fr.isen.buton.androidcontactds
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
+
+
     }
 }
